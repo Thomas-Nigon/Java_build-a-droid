d9ce6355f8be3d76c24e2d299492f8e89f751574
Thomas Nigon <[nigont@gmail.com](mailto:nigont@gmail.com)>
2024-04-10 14:44:31 +0200

    exercice done

--- Droid.java
+++ Droid.java
@@ -17,6 +17,15 @@ public class Droid{
         System.out.println( "energy report: my battery life is: " + batteryLevel + "%");
     }
 
+    public void energyTransfer(){
+      int missingBat = 100 - batteryLevel;
+System.out.println(name + " is transfering energy from backupDroid");
+System.out.println(name + " need " + missingBat +"% of battery");
+batteryLevel += missingBat;
+System.out.println(name + " is fully recharged");
+
+    }
+
     public void performtask(String task){
     System.out.println(name + " is performing a task: " + task);
     batteryLevel -= 10;
@@ -26,8 +35,12 @@ public class Droid{
     //Main
     public static void main(String[] args){
     Droid R2D2 = new Droid("R2D2");
+    Droid BackupBat = new Droid("BackupBat");
     System.out.println(R2D2);
+    System.out.println(BackupBat);
     R2D2.performtask("chill");
+    R2D2.energyReport();
+    R2D2.energyTransfer();
     R2D2.energyReport();
     }
   }
